4650f622509bc3c147bac2046d95b2145581b283
Samsung <makarkardash@gmail.com>
2024-01-25 21:53:42 +0300

    sohxofjs

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 def calculate_area(length, width):
     return length * width
 
-# Вводим значения длины и ширины с клавиатуры
 length = int(input("Введите длину прямоугольника: "))
 width = int(input("Введите ширину прямоугольника: "))
 
-# Вызываем функцию calculate_area и передаем ей значения длины и ширины
 area = calculate_area(length, width)
 
-# Выводим площадь прямоугольника на экран
 print("Площадь прямоугольника равна:", area)
