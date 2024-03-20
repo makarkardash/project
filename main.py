ab4a79e6791d6ef893c7d63f2ea93e7aa928c95f
Samsung <makarkardash@gmail.com>
2024-03-20 21:24:06 +0300

    sohxofjs

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# Нужно написать функцию для возврата квадрата переданного ей числа.
-def square(namber):
-    return namber ** 2
-namber = 15
-result = square(namber)
-print(f"Квадрат числа {namber} равен {result}")
+'''# Написать функцию для подсчёта факториала,
+# в качестве аргумента которой будет передаваться число
+
+
+a = 5
+out = 1
+for i in range(a):
+    out = out * (i + 1)
+print(out)
+
+b = 5
+
+
+def square(n):
+
+    return n ** 2
+
+
+out_square = square(b)
+print(out_square)'''
+
+
+def fakt(a):
+    out = 1
+    for i in range(a):
+        out = out * (i + 1)
+    return out
+print(fakt(5))
